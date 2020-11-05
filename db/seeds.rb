beec80a541184357f760c6239f20beeed2e22cfb
camille colet <camcolet@gmail.com>
2020-11-05 15:48:48 +0100

    beginned the front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "Lemon juice")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint leaves")
+Ingredient.create(name: "Aquafaba")
+Ingredient.create(name: "Basil Smash")
+Ingredient.create(name: "Angostura")
+Ingredient.create(name: "Simple syrup")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Whiskey")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Amaretto")
+
